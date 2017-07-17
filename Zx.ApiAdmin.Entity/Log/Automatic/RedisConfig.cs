5f54299d4d4cf5f1ea811ad6f6c7bd8cfe32169f
zxh <agrainofsand2011@126.com>
2017-07-17 10:12:53 +0800

    加入丢失log文件夹

---
+++ Zx.ApiAdmin.Entity/Log/Automatic/RedisConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zx.ApiAdmin.Entity.Attributes;
+
+namespace Zx.ApiAdmin.Entity.Log.Automatic
+{
+    [Serializable]
+    public class RedisConfig
+    {
+        [PrimaryKeyAttribute]
+        [IdentityAttribute]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Redis地址
+        /// </summary>
+        public string RedisHost { get; set; }
+
+        /// <summary>
+        /// Redis端口号
+        /// </summary>
+        public string RedisPort { get; set; }
+    }
+}
